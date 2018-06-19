c0ac14bbcfff14498312c8a81b6200cb354cbd9c
Yuriy Kirillov <yurkakirillov@gmail.com>
2018-06-19 21:43:14 +0300

    use pluck

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,5 +2,5 @@ class News < ApplicationRecord
   has_and_belongs_to_many :audiences
 
   scope :for_audiences, -> (audiences) { left_joins(:audiences)
-                           .where(audiences: { id: [audiences.map(&:id), nil] }) if audiences.present? }
+                           .where(audiences: { id: [audiences.pluck(:id), nil] }) if audiences.present? }
 end
